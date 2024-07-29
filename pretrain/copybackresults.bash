20bbcc9f8e10199303f7598b2e61cfee48476720
Evelyn Mannix <nathaniel.bloomfield@unimelb.edu.au>
2024-07-29 14:04:33 +1000

    added supporting files

---
+++ pretrain/copybackresults.bash
@@ -0,0 +1,5 @@
+
+rsync -avh nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/tests/cider/checkpoints .
+rsync -avh nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/tests/cider/save .
+rsync -avh nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/tests/cider/logs .
+
---
+++ pretrain/copyupcode.bash
@@ -0,0 +1,2 @@
+
+rsync -avh --exclude '.git'  --exclude '__pycache__' --exclude 'checkpoints'  . nbloomfield@spartan.hpc.unimelb.edu.au:/data/cephfs/punim0980/tests/cider
